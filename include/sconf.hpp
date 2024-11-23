d322e83c37b4ab1293ec5c9ef3fe41bb0f3a9c11
Nathanne Isip <nathanneisip@gmail.com>
2024-11-23 08:39:37 +0800

    sConf main library header file.

---
+++ include/sconf.hpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024 - Nathanne Isip
+ * This file is part of sConf.
+ * 
+ * N8 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * N8 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with N8. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file sconf.hpp
+ * @author [Nathanne Isip](https://github.com/nthnn)
+ * @brief Main header file for the sConf library.
+ *
+ * The sConf library provides utilities for handling structured configuration 
+ * files, including parsing, manipulation, and error handling. This header file 
+ * aggregates all the core components of the library, making it convenient for 
+ * inclusion in projects that utilize sConf.
+ */
+#ifndef SCONF_HPP
+#define SCONF_HPP
+
+#include <sconf_exception.hpp>
+#include <sconf_value.hpp>
+#include <sconf_parser.hpp>
+
+#endif
